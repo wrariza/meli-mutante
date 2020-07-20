ae4dd81453691e16a08d0c4538c4dd8b9c7176ff
william ricardo ariza velez <wilarizav@gmail.com>
2020-07-20 18:06:06 -0500

    feat: implement stats enpoint

---
+++ src/domain/models/stats/Stats.model.ts
@@ -0,0 +1,7 @@
+export class Stats {
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  count_mutant_dna: number
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  count_human_dna: number
+  ratio: number
+}
