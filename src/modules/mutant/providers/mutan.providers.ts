43d11bcfff51cf6168c24cd7c4973530f1f0c789
william ricardo ariza velez <wilarizav@gmail.com>
2020-07-20 14:39:17 -0500

    feat: implement save dna

--- src/modules/mutant/providers/mutan.providers.ts
+++ src/modules/mutant/providers/mutan.providers.ts
@@ -1,10 +1,18 @@
 // Inject
-import { SMutan } from '../../../bin/constants_injection'
+import { SMutan, RMutan } from '../../../bin/constants_injection'
 
 // Service
 import { MutantService } from '../service/mutant.service'
 
+// Repository
+import { MutantRepository } from '../repository/mutant.repository'
+
 export const serviceMutanProvider = {
   provide: SMutan,
   useClass: MutantService
+}
+
+export const repositoryProvider = {
+  provide: RMutan,
+  useClass: MutantRepository
 }
--- src/modules/mutant/service/mutant.service.spec.ts
+++ src/modules/mutant/service/mutant.service.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { MutantService } from './mutant.service'
+import { RMutan } from '../../../bin/constants_injection'
+import { MutantRepository } from '../repository/mutant.repository'
 
 describe('MutantService', () => {
   let service: MutantService
+  let repository: MutantRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MutantService]
+      providers: [
+        MutantService,
+        {
+          provide: RMutan,
+          useValue: {
+            create: () => {
+              return {}
+            }
+          }
+        }
+      ]
     }).compile()
 
     service = module.get<MutantService>(MutantService)
+    repository = module.get<MutantRepository>(RMutan)
+
+    jest.spyOn(repository, 'create').mockImplementation()
   })
 
   it('should be defined', () => {
